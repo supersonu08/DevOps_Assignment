a832cbbea08f25637c2e117dbf271365e79c7db0
Naveen <sonupargi58@gmail.com>
2026-04-03 00:32:51 +0530

    assignment done

--- src/test/java/com/devopsAssignment/crossBrowserTesting.java
+++ src/test/java/com/devopsAssignment/crossBrowserTesting.java
@@ -18,42 +18,39 @@ import org.testng.annotations.Test;
 
 public class crossBrowserTesting {
 	WebDriver driver;
-  @Parameters({"browser"}) 
-  @Test
-  public void broswerTest(String browser) throws InterruptedException, MalformedURLException, URISyntaxException {
-	  
-	  if(browser.equalsIgnoreCase("Chrome"))
-	  {
-		  ChromeOptions options = new ChromeOptions();
-		  driver = new RemoteWebDriver(new URI("http://localhost:4444").toURL(),options);
-		  System.out.println("chrome launched");
-	  }
-	  
-	  else if(browser.equalsIgnoreCase("FireFox"))
-	  {
-		  FirefoxOptions options = new FirefoxOptions();
-		  driver = new RemoteWebDriver(new URI("http://localhost:4444").toURL(),options);
-		  
-		  System.out.println("firefox launched");
-	  }
-	  
-	  else if(browser.equalsIgnoreCase("Edge"))
-	  {
-		  EdgeOptions options = new EdgeOptions();
-		  driver = new RemoteWebDriver(new URI("http://localhost:4444").toURL(),options);
-		  System.out.println("edge launched");
-	  }
-	  
-	  else { 
-		  System.out.println("Invalid borwser");
-	  }
-	  
-	 
-	  driver.get("https://www.amazon.com/");	  
-	  Thread.sleep(2000);	 
-	  String acttitle = driver.getTitle();
-	 	  
-	  AssertJUnit.assertTrue(acttitle.contains("Amazon"));	 
-	  driver.quit();
-  }
+
+	@Parameters({ "browser" })
+	@Test
+	public void broswerTest(String browser) throws InterruptedException, MalformedURLException, URISyntaxException {
+
+		if (browser.equalsIgnoreCase("Chrome")) {
+			ChromeOptions options = new ChromeOptions();
+			driver = new RemoteWebDriver(new URI("http://localhost:4444").toURL(), options);
+			System.out.println("chrome launched");
+		}
+
+		else if (browser.equalsIgnoreCase("FireFox")) {
+			FirefoxOptions options = new FirefoxOptions();
+			driver = new RemoteWebDriver(new URI("http://localhost:4444").toURL(), options);
+
+			System.out.println("firefox launched");
+		}
+
+		else if (browser.equalsIgnoreCase("Edge")) {
+			EdgeOptions options = new EdgeOptions();
+			driver = new RemoteWebDriver(new URI("http://localhost:4444").toURL(), options);
+			System.out.println("edge launched");
+		}
+
+		else {
+			System.out.println("Invalid borwser");
+		}
+
+		driver.get("https://www.amazon.com/");
+		Thread.sleep(2000);
+		String acttitle = driver.getTitle();
+
+		AssertJUnit.assertTrue(acttitle.contains("Amazon"));
+		driver.quit();
+	}
 }
